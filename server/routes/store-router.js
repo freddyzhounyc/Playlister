1c47ef106788f4c827d5ddad0e9344e4e12bfe1a
Freddy Zhou <freddyzhou06@gmail.com>
2025-12-07 14:07:09 -0500

    Add display of songs in a playlist.

--- server/routes/store-router.js
+++ server/routes/store-router.js
@@ -3,10 +3,25 @@ const router = express.Router();
 const auth = require('../auth/index');
 const UserController = require('../controllers/user-controller');
 const PlaylistController = require('../controllers/playlist-controller');
+const SongController = require('../controllers/song-controller');
+const PlaylistSongController = require('../controllers/playlistSong-controller');
 
+// User Controller
 router.put("/user/:id", auth.verify, UserController.updateUser);
+
+// Playlist Controller
 router.get("/playlistpairs", PlaylistController.getPlaylistPairs); // Public Endpoint
 router.get("/userByPlaylistId/:id", PlaylistController.getUserByPlaylistId); // Public Endpoint
 router.post("/playlist", auth.verify, PlaylistController.createPlaylist);
+router.get("/playlist/:id", PlaylistController.getPlaylistById) // Public Endpoint
+router.get("/playlists", PlaylistController.getPlaylists) // Public Endpoint
+
+// PlaylistSong Controller
+router.get("/songsInPlaylist/:id", PlaylistSongController.getSongsInPlaylist) // Public Endpoint
+router.post("/playlistSong", auth.verify, PlaylistSongController.createPlaylistSong);
+
+// Song Controller
+router.post("/song", auth.verify, SongController.createSong);
+router.get("/song/:id", auth.verify, SongController.getSongById);
 
 module.exports = router;
